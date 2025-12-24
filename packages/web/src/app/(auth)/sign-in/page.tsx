78c25059c525abbf5c0bce2db1c4dcf98102a026
MikaelMarceniuk <mika.marceniuk@gmail.com>
2025-12-24 17:55:21 +0000

    feat(web): Created sign-in page

---
+++ packages/web/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,24 @@
+import { IconTimeline } from '@tabler/icons-react'
+import { SignInForm } from './_components/sign-in.form'
+
+const SignInPage = () => {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a
+          href="#"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <IconTimeline />
+          </div>
+          Spec Ops.
+        </a>
+
+        <SignInForm />
+      </div>
+    </div>
+  )
+}
+
+export default SignInPage
--- packages/web/src/components/ui/button.tsx
+++ packages/web/src/components/ui/button.tsx
@@ -3,9 +3,10 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { Slot } from 'radix-ui'
 
 import { cn } from '@/src/lib/utils'
+import { Spinner } from './spinner'
 
 const buttonVariants = cva(
-  "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 rounded-md border border-transparent bg-clip-padding text-sm font-medium focus-visible:ring-[3px] aria-invalid:ring-[3px] [&_svg:not([class*='size-'])]:size-4 inline-flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none group/button select-none",
+  "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 rounded-md border border-transparent bg-clip-padding text-sm font-medium focus-visible:ring-[3px] aria-invalid:ring-[3px] [&_svg:not([class*='size-'])]:size-4 inline-flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none group/button select-none cursor-pointer",
   {
     variants: {
       variant: {
@@ -41,16 +42,23 @@ const buttonVariants = cva(
   }
 )
 
+type ButtonProps = {
+  isLoading?: boolean
+} & React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }
+
 function Button({
   className,
   variant = 'default',
   size = 'default',
   asChild = false,
+  isLoading = false,
+  children,
+  disabled,
   ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+}: ButtonProps) {
   const Comp = asChild ? Slot.Root : 'button'
 
   return (
@@ -58,9 +66,15 @@ function Button({
       data-slot="button"
       data-variant={variant}
       data-size={size}
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(
+        'inline-flex items-center gap-2', // garante alinhamento horizontal
+        buttonVariants({ variant, size, className })
+      )}
+      disabled={isLoading || disabled}
       {...props}
-    />
+    >
+      {isLoading ? <Spinner /> : children}
+    </Comp>
   )
 }
 
