90984d004e7e14e1d02bbc461c22abfffb525255
MikaelMarceniuk <mika.marceniuk@gmail.com>
2025-12-25 23:59:14 +0000

    feat(auth): add email/password authentication

--- packages/server/src/infra/auth.infra.ts
+++ packages/server/src/infra/auth.infra.ts
@@ -9,4 +9,20 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
+  advanced: {
+    database: {
+      generateId: false,
+    },
+    disableOriginCheck: true,
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    minPasswordLength: 8,
+    password: {
+      hash: async (password) => await Bun.password.hash(password),
+      verify: async ({ password, hash }) =>
+        await Bun.password.verify(password, hash),
+    },
+  },
 })
